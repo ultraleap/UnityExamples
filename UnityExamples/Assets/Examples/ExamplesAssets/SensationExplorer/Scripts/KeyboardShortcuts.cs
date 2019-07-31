73fdc616ac32fb145ede62e2cbb04727ace74d53
AntonyNasce-UH <antony.nasce@ultrahaptics.com>
2019-07-31 09:46:14 +0100

    Control points and Array visibility can be toggled with F-H keys

--- UnityExamples/Assets/Examples/ExamplesAssets/SensationExplorer/Scripts/KeyboardShortcuts.cs
+++ UnityExamples/Assets/Examples/ExamplesAssets/SensationExplorer/Scripts/KeyboardShortcuts.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class KeyboardShortcuts : MonoBehaviour
 {
+    public GameObject focalPointModel;
+    public GameObject kitModel;
+    public GameObject trackerModel;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,6 +20,17 @@ public class KeyboardShortcuts : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             Application.Quit();
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            focalPointModel.SetActive(!focalPointModel.active);
+        }
+
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            kitModel.SetActive(!kitModel.active);
+            trackerModel.SetActive(!trackerModel.active);
         }
     }
 }
