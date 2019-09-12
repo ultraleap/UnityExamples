845a5c45c0fd108cfc5453cd99f71dbe62bd030e
Antony Nasce <antony.nasce@ultrahaptics.com>
2019-09-12 16:41:03 +0100

    Add a basic build script for builds.

---
+++ UnityExamples/Assets/Examples/ExamplesAssets/SensationExplorer/Editor/BuildSensationExplorer.cs
@@ -0,0 +1,77 @@
+﻿using UnityEditor;
+using UnityEngine;
+using UnityEditor.Build.Reporting;
+using System;
+
+using System.Collections.Generic;
+using System.IO;
+
+// Output the build size or a failure depending on BuildPlayer.
+public class BuildSensationExplorer : MonoBehaviour
+{
+	[MenuItem("UnityExamples/Sensation Explorer/Build for Mac")]
+	public static void BuildMacOS()
+	{
+		BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+		buildPlayerOptions.scenes = new[] { "Assets/Examples/SensationExplorer.unity" };
+
+		buildPlayerOptions.target = BuildTarget.StandaloneOSX;
+		buildPlayerOptions.options = BuildOptions.None;
+
+		var buildDir = "/tmp/";
+		var productName = PlayerSettings.productName;
+        var productVersion = PlayerSettings.bundleVersion;
+		var appName = productName + productVersion + ".app";
+		var buildPath = buildDir + appName;
+
+        Debug.Log("Build App name is:" + buildPath);
+
+        buildPlayerOptions.locationPathName = buildPath;
+
+        BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+		BuildSummary summary = report.summary;
+
+		if (summary.result == BuildResult.Succeeded)
+		{
+			Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+		}
+
+		if (summary.result == BuildResult.Failed)
+		{
+			Debug.Log("Build failed");
+		}
+	}
+
+    [MenuItem("UnityExamples/Sensation Explorer/Build for Windows")]
+    public static void BuildWindows()
+    {
+        BuildPlayerOptions buildPlayerOptions = new BuildPlayerOptions();
+        buildPlayerOptions.scenes = new[] { "Assets/Examples/SensationExplorer.unity" };
+
+        buildPlayerOptions.target = BuildTarget.StandaloneWindows64;
+        buildPlayerOptions.options = BuildOptions.None;
+
+        var buildDir = "C:\temp";
+        var productName = PlayerSettings.productName;
+        var productVersion = PlayerSettings.bundleVersion;
+        var appName = productName + productVersion + ".exe";
+        var buildPath = buildDir + appName;
+
+        Debug.Log("Build App name is:" + buildPath);
+
+        buildPlayerOptions.locationPathName = buildPath;
+
+        BuildReport report = BuildPipeline.BuildPlayer(buildPlayerOptions);
+        BuildSummary summary = report.summary;
+
+        if (summary.result == BuildResult.Succeeded)
+        {
+            Debug.Log("Build succeeded: " + summary.totalSize + " bytes");
+        }
+
+        if (summary.result == BuildResult.Failed)
+        {
+            Debug.Log("Build failed");
+        }
+    }
+}
