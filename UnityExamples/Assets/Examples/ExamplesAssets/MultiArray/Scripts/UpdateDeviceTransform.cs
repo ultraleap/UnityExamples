dce8c0591f657d8f7e0fee44183dae157b10bb2e
AntonyNasce-UH <antony.nasce@ultrahaptics.com>
2019-09-17 12:30:05 +0100

    Update MultiArray Example and hand outlines in preparation for UCA Beta 9.

--- UnityExamples/Assets/Examples/ExamplesAssets/MultiArray/Scripts/UpdateDeviceTransform.cs
+++ UnityExamples/Assets/Examples/ExamplesAssets/MultiArray/Scripts/UpdateDeviceTransform.cs
@@ -17,19 +17,9 @@ public class UpdateDeviceTransform : MonoBehaviour {
         if (transform.hasChanged)
         {
             print("The Device transform with deviceID: " + deviceID + " has changed: position: " + transform.position + " , rotation: " + transform.rotation);
-            var tmpTransform = new GameObject().transform;
-            tmpTransform.gameObject.hideFlags = HideFlags.HideAndDontSave;
-
-            // To use Unity Transform directly to represent the device transform we currently have to swap Y-Z coordinates.
-            Vector3 rotationVector = new Vector3(transform.eulerAngles.x, transform.eulerAngles.z, transform.eulerAngles.y);
-            Vector3 positionVector = new Vector3(transform.localPosition.x, transform.localPosition.z, transform.localPosition.y);
-
-            Quaternion rotation = Quaternion.Euler(rotationVector);
-            tmpTransform.SetPositionAndRotation(positionVector, rotation);
 
             // Update the Device Transform
-            SensationCore.Instance.SetDeviceTransform(deviceID, tmpTransform);
-            Destroy(tmpTransform.gameObject);
+            SensationCore.Instance.SetDeviceTransform(deviceID, transform);
 
             transform.hasChanged = false;
         }
