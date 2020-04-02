ecf03c3e55f7b6a1dab9448f736641bf510194ad
Antony Nasce <antony.nasce@ultraleap.com>
2020-04-02 14:02:57 +0100

    PointAM update - better approximation to an Amplitude Modulation point, using the path rendering pipeline.

--- UnityExamples/Assets/StreamingAssets/Python/BlockLibraries/UnityExamples/PointAM.py
+++ UnityExamples/Assets/StreamingAssets/Python/BlockLibraries/UnityExamples/PointAM.py
@@ -1,26 +1,26 @@
+# An approximation to an Amplitude Modulation point, which uses the path rendering pipeline.
+# This makes it more convenient to transform a point, via Unity Transfrom in the inspector.
 from pysensationcore import *
 import sensation_helpers as sh
 import IntensityWave
 
 path = createInstance("CirclePath","innerPath")
+
+# We use a Circle Path of zero radius
+connect(Constant((0,0,0)), path.radius)
 cosineWaveInstance = createInstance("IntensityWave","IntensityWaveInstance")
 
 pointAM = sh.createSensationFromPath("PointAM",
                                   {
-                                      ("size", path.radius) : (0.005, 0, 0),
                                       ("amFrequency", cosineWaveInstance.modulationFrequency) : (143, 0, 0),
                                       ("t", cosineWaveInstance.t) : (0, 0, 0)
                                   },
                                   output = path.out,
                                   intensity = cosineWaveInstance.out,
-                                  drawFrequency = 70,
+                                  drawFrequency = 0,
                                   renderMode=sh.RenderMode.Loop
                                   )
 
 setMetaData(pointAM, "Allow-Transform", True)
-setMetaData(pointAM.size, "Type", "Scalar")
-setMetaData(pointAM.size, "Input-Visibility", False)
-setMetaData(pointAM.size, "Type", "Scalar")
-setMetaData(pointAM.size, "Input-Visibility", False)
 setMetaData(pointAM.amFrequency, "Type", "Scalar")
-setMetaData(pointAM.amFrequency, "Input-Visibility", False)
+setMetaData(pointAM.drawFrequency, "Input-Visibility", False)
